c28e3c16148a6ed278ad307120d9a2d1e457d285
Deepakraghavan01 <57013229+Deepakraghavan01@users.noreply.github.com>
2021-06-07 23:02:27 +0530

    Add files via upload

---
+++ test_web/urls.py
@@ -0,0 +1,37 @@
+"""test_web URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from web_test import views
+
+urlpatterns = [
+    path(r'',views.home),
+    path(r'Admin/',views.Admin),
+    path(r'Admin/question_dash/',views.question_dash),
+    path(r'Admin/add_student/',views.add_student),
+    path(r'Admin/stu_det/',views.stu_det),
+    path(r'Admin/question_add/',views.question_add),
+    path(r'Admin/add/',views.add),
+    path(r'Admin/db/',views.db),
+    path(r'Admin/view_question/',views.view_question),
+    path(r'Admin/Exam/',views.Exam),
+    path(r'Admin/save_exam/',views.save_exam),
+    path(r'Student/',views.Student),
+    path(r'Student/student_dash/',views.student_dash),
+    path(r'Student/student_exam/',views.student_exam),
+    path(r"Student/student_mark/",views.student_mark),
+
+]
