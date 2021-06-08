4904e7c7493c00ae645db39a0e407de793719efc
Deepakraghavan01 <57013229+Deepakraghavan01@users.noreply.github.com>
2021-06-08 17:31:26 +0530

    Add files via upload

--- test_web/settings.py
+++ test_web/settings.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'test_web.urls'
 
 TEMPLATES = [
-    {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
@@ -122,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
